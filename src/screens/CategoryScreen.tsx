5c47845536320031f4122de8a1d30b2a985b03b2
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2023-06-06 21:35:30 -0300

    Add loader component to Category screen

--- src/screens/CategoryScreen.tsx
+++ src/screens/CategoryScreen.tsx
@@ -4,15 +4,19 @@ import { useState, useEffect, useCallback  } from 'react';
 import { Product } from '../types';
 import { getProductsByCategory } from '../services/api';
 import ProductList from '../components/ProductList';
+import LoaderComponent from '../components/LoaderComponent';
 
 export default function CategoryScreen ({ route }: { route: RouteProp<any> })  {
     const { category }: any = route.params;
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
 
     const fetchProductsByCategory = useCallback(async () => {
         try {
+          setLoading(true);
           const products = await getProductsByCategory(category.strCategory);
           setProducts(products);
+          setLoading(false);
         } catch (error) {
           console.error('Error fetching products by category:', error);
         } 
@@ -24,7 +28,11 @@ export default function CategoryScreen ({ route }: { route: RouteProp<any> })  {
   
     return (
         <View style={style.container}>
+          {
+            loading ? <LoaderComponent /> 
+          : 
             <ProductList products={products} />
+          }
         </View>
     );
     }
